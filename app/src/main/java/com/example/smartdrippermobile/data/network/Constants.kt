dc6f565e26e717d6f990379f060fc3f87166a38b
George Svitenko <georg.svit@gmail.com>
2020-12-23 01:19:22 +0200

    Routes for appointment added

--- app/src/main/java/com/example/smartdrippermobile/data/network/Constants.kt
+++ app/src/main/java/com/example/smartdrippermobile/data/network/Constants.kt
@@ -3,4 +3,6 @@ package com.example.smartdrippermobile.data.network
 object Constants {
     const val BASE_URL = "http://10.0.2.2:44354/"
     const val LOGIN_URL = "nurses/login"
+    const val APPOINTMENTS_URL = "appointments"
+    const val DONE_URL = "done/{appointmentId}"
 }
