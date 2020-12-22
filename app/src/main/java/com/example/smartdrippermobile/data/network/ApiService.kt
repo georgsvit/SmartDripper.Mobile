4707b2df1968c04f9a9ca1fef64b8c8ecda38c44
George Svitenko <georg.svit@gmail.com>
2020-12-23 01:23:10 +0200

    API Service methods for appointment added

--- app/src/main/java/com/example/smartdrippermobile/data/network/ApiService.kt
+++ app/src/main/java/com/example/smartdrippermobile/data/network/ApiService.kt
@@ -2,9 +2,16 @@ package com.example.smartdrippermobile.data.network
 
 import com.example.smartdrippermobile.data.network.dto.requests.*
 import com.example.smartdrippermobile.data.network.dto.responses.*
+import okhttp3.ResponseBody
 import retrofit2.http.*
 
 interface ApiService {
     @POST(Constants.LOGIN_URL)
     suspend fun login(@Body request: LoginRequest) : JWTTokenResponse
+
+    @GET(Constants.APPOINTMENTS_URL)
+    suspend fun getAppointments(@Header("Authorization") token: String) : List<AppointmentResponse>
+
+    @POST(Constants.APPOINTMENTS_URL + Constants.DONE_URL)
+    suspend fun setAppointmentDone(@Path("appointmentId") appointmentId: String, @Header("Authorization") token: String) : ResponseBody
 }
